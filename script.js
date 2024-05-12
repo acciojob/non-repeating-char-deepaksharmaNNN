e6e95171e2b6fabc6e0721e7ee3792aa153631da
Deepak Sharma <dsharma2828@gmail.com>
2024-05-12 11:06:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
+    let set = new Set();
+    for(let i = 0; i < str.length; i++) {
+        if(set.has(str[i])) {
+            return str[i];
+        }
+        set.add(str[i]);
+    }
+    return null;
 }
-const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+
+// const input = prompt("Enter a string");
+// alert(firstNonRepeatedChar(input)); 
